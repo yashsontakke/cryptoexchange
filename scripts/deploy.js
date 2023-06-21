64ff0f754433eee6cd99aee32e4fffc871f0ea9d
Yash Shivajirao Sontakke <beingyash@Yashs-MacBook.local>
2023-06-21 21:22:03 +0530

    deployed contracts

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,13 +7,21 @@
 const hre = require("hardhat");
 async function main() {
   const Token = await hre.ethers.getContractFactory("Token");
-  const token = await Token.deploy("DappToken", "Dapp");
-  await token.deployed();
+  const Exchange= await hre.ethers.getContractFactory("Exchange");
+  const accounts = await  ethers.getSigners();
 
-  console.log(
-    `Contract deployed to ${token.address}`
-    // 0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512
-  );
+  const dapp = await Token.deploy("Dapp","Dapp","1000000");
+  const mEth = await Token.deploy("mETH","mETH","1000000");
+  const mDai = await Token.deploy("mDAI","mDAI","1000000");
+
+ 
+  // mEth deployed to 0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512
+  // mDai deployed to 0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0
+  
+  const exchange = await Exchange.deploy(accounts[1].address,10);
+  // exchange deployed to 0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9
+
+
 }
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.

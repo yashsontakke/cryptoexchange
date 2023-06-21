7a2313f65b3c71d6602410b0c3e90f6ffdbcfa2f
Yash Shivajirao Sontakke <beingyash@Yashs-MacBook.local>
2023-06-21 22:15:33 +0530

    Added config and seed exchange file

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -14,7 +14,7 @@ async function main() {
   const mEth = await Token.deploy("mETH","mETH","1000000");
   const mDai = await Token.deploy("mDAI","mDAI","1000000");
 
- 
+  // dapp deplyed to 0x5FbDB2315678afecb367f032d93F642f64180aa3
   // mEth deployed to 0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512
   // mDai deployed to 0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0
   
